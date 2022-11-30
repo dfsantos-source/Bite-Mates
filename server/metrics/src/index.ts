b515b5dff51a1fcfac027843039c487c7bbbc773
Dane Santos <dsantos@umass.edu>
2022-11-29 21:58:46 -0500

    Added docker-compose | For each service, added Dockerfile, initialized npm environment, installed libraries, added tsconfig.json

---
+++ server/metrics/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 4005;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'ok' });
+});
+
+app.listen(port, () => {
+  console.log(`Running on ${port}.`);
+});
