5369f8d9b036c35d5ad2bd7e7602f2474350cf82
Aayush Bhagat <lamblesus@gmail.com>
2022-12-11 23:36:04 -0500

    created frontend client

---
+++ client/pages/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function login() {
+  return (
+    <div>
+          <div>login</div>
+        <input type="text" name="" id="" placeholder='email'/>
+    </div>
+  )
+}
